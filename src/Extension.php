7ae8711b4b7e0d8cba6217538a8643bd5f7434dd
king east <1207877378@qq.com>
2020-12-14 02:26:48 +0800

    load增加version

--- src/Extension.php
+++ src/Extension.php
@@ -11,13 +11,19 @@
 namespace ke;
 
 
+use think\facade\App;
+
 class Extension extends \Twig_Extension
 {
+    protected $isDebug = false;
+
+
     protected $config = [];
 
 
     public function __construct($config)
     {
+        $this->isDebug =App::isDebug();
         $this->config = $config;
     }
 
@@ -32,7 +38,10 @@ class Extension extends \Twig_Extension
             /**
              * 加载资源文件
              */
-            new \Twig_Function('load', function ($type, $str) {
+            new \Twig_Function('load', function ($type, $str, $noCache = false) {
+                if (!$noCache && $this->isDebug) {
+                    $str .= '?v=' . time();
+                }
                 $load = function ($str) use($type) {
                     $str = $this->config['asset_path'] . $str;
                     if ($type === 'js') {
